cb8bc1ea0e162b96175f9cade0bad2713508d4ba
a132395 <67187730+a132395@users.noreply.github.com>
2020-12-03 13:51:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ async function updateDB(){
     ON CONFLICT 
     DO NOTHING;`;
 
-    for (let i = 1 ; i<= 10 ; i++){
+    for (let i = 1 ; i<= 1 ; i++){
         //
         const promises = [];
         try{
